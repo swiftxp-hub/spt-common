ff5ae111abcf7b4109ca985d7efb3b946a90e46c
Sebastian Foot <230956466+swiftxp-hub@users.noreply.github.com>
2026-01-27 12:00:47 +0100

    AppMetadata.Version added for baking version into my spt-mods.

---
+++ Runtime/AppMetadata.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+
+namespace SwiftXP.SPT.Common.Runtime;
+
+public static class AppMetadata
+{
+    public static string Version =>
+        Assembly
+            .GetExecutingAssembly()
+            ?.GetName()
+            .Version
+            ?.ToString(3) ?? "0.0.0";
+}
