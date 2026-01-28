ae6d41a39df4cd8f59df0712c92f6092047c6a3c
Sebastian Foot <230956466+swiftxp-hub@users.noreply.github.com>
2026-01-28 15:00:35 +0100

    FileSearchService now uses FileSystemGlobbing from MS. Removed obsolete PathStringExtensions.

--- Services/FileSearchService.cs
+++ Services/FileSearchService.cs
@@ -1,7 +1,9 @@
 using global::System;
 using global::System.Collections.Generic;
 using global::System.IO;
-using SwiftXP.SPT.Common.Extensions;
+using global::System.Linq;
+using global::Microsoft.Extensions.FileSystemGlobbing;
+using global::Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using SwiftXP.SPT.Common.Services.Interfaces;
 
 namespace SwiftXP.SPT.Common.Services;
@@ -13,94 +15,18 @@ public class FileSearchService : IFileSearchService
 {
     public IEnumerable<string> GetFiles(string baseDirectory, IEnumerable<string> pathsToSearch, IEnumerable<string> pathsToExclude)
     {
-        HashSet<string> searchRoots = ResolvePaths(baseDirectory, pathsToSearch);
-
-        (HashSet<string>? excludedFiles, List<string>? excludedDirectories) = ParseExclusions(baseDirectory, pathsToExclude);
-
-        foreach (string searchPath in searchRoots)
+        if (string.IsNullOrEmpty(baseDirectory) || !Directory.Exists(baseDirectory))
         {
-            if (File.Exists(searchPath))
-            {
-                if (!IsExcluded(searchPath, excludedFiles, excludedDirectories))
-                {
-                    yield return searchPath.ToUnixStylePath();
-                }
-            }
-            else if (Directory.Exists(searchPath))
-            {
-                IEnumerable<string> files = Directory.EnumerateFiles(searchPath, "*", SearchOption.AllDirectories);
-
-                foreach (string file in files)
-                {
-                    if (IsExcluded(file, excludedFiles, excludedDirectories))
-                    {
-                        continue;
-                    }
-
-                    yield return file.ToUnixStylePath();
-                }
-            }
-        }
-    }
-
-    private static HashSet<string> ResolvePaths(string baseDirectory, IEnumerable<string> relativePaths)
-    {
-        HashSet<string> result = new(StringComparer.OrdinalIgnoreCase);
-
-        foreach (string relativePath in relativePaths)
-        {
-            string normalizedPath = relativePath.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
-            string fullPath = Path.GetFullPath(normalizedPath, baseDirectory);
-
-            result.Add(fullPath);
+            return [];
         }
 
-        return result;
-    }
+        Matcher matcher = new(StringComparison.OrdinalIgnoreCase);
+        matcher.AddIncludePatterns(pathsToSearch);
+        matcher.AddExcludePatterns(pathsToExclude);
 
-    private static (HashSet<string> Files, List<string> Directories) ParseExclusions(string baseDirectory, IEnumerable<string> pathsToExclude)
-    {
-        HashSet<string> files = new(StringComparer.OrdinalIgnoreCase);
-        List<string> directories = [];
+        DirectoryInfo directoryInfo = new(baseDirectory);
+        PatternMatchingResult result = matcher.Execute(new DirectoryInfoWrapper(directoryInfo));
 
-        foreach (string path in pathsToExclude)
-        {
-            string normalizedPath = path.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar);
-            string fullPath = Path.GetFullPath(normalizedPath, baseDirectory);
-
-            if (Directory.Exists(fullPath))
-            {
-                directories.Add(fullPath);
-            }
-            else
-            {
-                files.Add(fullPath);
-            }
-        }
-
-        return (files, directories);
-    }
-
-    private static bool IsExcluded(string filePath, HashSet<string> excludedFiles, List<string> excludedDirectories)
-    {
-        if (excludedFiles.Contains(filePath))
-            return true;
-
-        foreach (string dir in excludedDirectories)
-        {
-            if (filePath.StartsWith(dir, StringComparison.OrdinalIgnoreCase))
-            {
-                int dirLen = dir.Length;
-                if (filePath.Length > dirLen && IsDirectorySeparator(filePath[dirLen]))
-                    return true;
-            }
-        }
-
-        return false;
-    }
-
-    private static bool IsDirectorySeparator(char c)
-    {
-        return c == Path.DirectorySeparatorChar || c == Path.AltDirectorySeparatorChar;
+        return result.Files.Select(file => Path.GetFullPath(Path.Combine(baseDirectory, file.Path)));
     }
 }
